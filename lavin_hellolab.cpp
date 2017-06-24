ecc6dd56705971d0018617bf5dfedc8e2f46150b
kllavin2 <kllavin2@gmail.com>
2017-06-24 12:57:43 -0400

    Add files via upload

---
+++ lavin_hellolab.cpp
@@ -0,0 +1,18 @@
+//Kristi Lavin
+//02/01/2017
+//Program name: Hello
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl << endl << "I Love C++" << endl;
+
+	cout << "I have three dogs!" << endl;
+
+	system("Pause");
+
+	return 0;
+
+}
